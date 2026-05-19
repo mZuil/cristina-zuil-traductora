9ac2c429879d0713eb61a6f0de67140e05c33db6
mZuil <maria20zg00@gmail.com>
2026-05-19 20:06:21 +0200

    Mailbox model added

--- cristina-zuil-traductora/src/env.d.ts
+++ cristina-zuil-traductora/src/env.d.ts
@@ -1,3 +1,7 @@
 /// <reference types="astro/client" />
 
 declare module 'virtual:astro:assets/fonts/internal';
+
+declare module 'three/examples/jsm/loaders/GLTFLoader.js';
+declare module 'three/examples/jsm/loaders/DRACOLoader.js';
+declare module 'three/examples/jsm/environments/RoomEnvironment.js';
