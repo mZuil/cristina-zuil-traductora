388d525d64d1d8e363993f1ccd9a51f47f4a285a
mZuil <maria20zg00@gmail.com>
2026-05-28 22:55:57 +0200

    add cloudflare adapter

--- cristina-zuil-traductora/astro.config.mjs
+++ cristina-zuil-traductora/astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import { i18nConfig } from './src/config/i18n';
+import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   i18n: {
@@ -10,4 +11,6 @@ export default defineConfig({
       prefixDefaultLocale: false,
     },
   },
+  output: 'server',
+  adapter: cloudflare(),
 });
