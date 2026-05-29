f236145dfd0fe2995019f223437d609bf8bac4ae
mZuil <maria20zg00@gmail.com>
2026-05-29 16:22:29 +0200

    first try

--- cristina-zuil-traductora/astro.config.mjs
+++ cristina-zuil-traductora/astro.config.mjs
@@ -2,8 +2,10 @@
 import { defineConfig } from 'astro/config';
 import { i18nConfig } from './src/config/i18n';
 import cloudflare from '@astrojs/cloudflare';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+  site: 'https://cristina-zuil-traductora.es',
   i18n: {
     defaultLocale: i18nConfig.defaultLocale,
     locales: i18nConfig.locales,
@@ -13,4 +15,14 @@ export default defineConfig({
   },
   output: 'server',
   adapter: cloudflare(),
+  integrations: [
+    sitemap({
+      i18n: {
+        defaultLocale: i18nConfig.defaultLocale,
+        locales: Object.fromEntries(
+          i18nConfig.locales.map((locale) => [locale, locale])
+        ),
+      },
+    }),
+  ],
 });
